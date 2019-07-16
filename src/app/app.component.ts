74e6c35cc1d6d13702b58153e6a465f6ba55ad92
David Valentine <DValentine@ethosgroup.com>
2019-07-16 18:05:05 -0500

    initial POC using subjects vs. global vars

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, zip, forkJoin, AsyncSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'subjectKings';
+
+  // pretend these are services.
+  public observable1: Observable<number> = new Observable(subscriber => {
+    setTimeout(() => {
+      subscriber.next(1);
+      subscriber.complete();
+    },1000);
+  });
+
+
+  public observable2: Observable<string> = new Observable(subscriber => {
+    setTimeout(() => {
+      subscriber.next('2');
+      subscriber.complete();
+    },1000);
+  });
+
+  public observable3: Observable<number> = new Observable(subscriber => {
+    setTimeout(() => {
+      subscriber.next(3);
+      subscriber.complete();
+    },1000);
+  });
+
+
+  ngOnInit(): void {
+    this.currentWayWeDoThings();
+    this.newWayToDoThings();
+  }
+
+  ///current pattern
+  public globalVar1: number;
+  public globalVar2: string;
+  public globalVar3: number;
+
+  currentWayWeDoThings() {
+    this.observable1.subscribe(a => {
+      this.globalVar1 = a;
+      // pretend a is needed to make requests to b and c
+      forkJoin(this.observable2, this.observable3).subscribe(([b, c]) => {
+        this.globalVar2 = b;
+        this.globalVar3 = c;
+      });
+    });
+  }
+
+  pressButton() {
+    console.log(this.globalVar1 + this.globalVar3);
+  }
+
+  //end current pattern
+  //new pattern
+  public subject1: AsyncSubject<number> = new AsyncSubject<number>();
+  public subject2: AsyncSubject<string>= new AsyncSubject<string>();
+  public subject3: AsyncSubject<number>= new AsyncSubject<number>();
+
+  newWayToDoThings() {
+    this.observable1.subscribe(this.subject1);
+
+    this.subject1.subscribe(z => {
+      this.observable2.subscribe(this.subject2);
+      this.observable3.subscribe(this.subject3);
+    });
+  }
+
+  pressButton2() {
+    zip(this.subject1, this.subject3).subscribe(([z, y]) => {
+      console.log(z + y);
+    });
+  }
+
+//end new pattern
+
 }
